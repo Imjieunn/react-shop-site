b523a60d8ae0ea9fbc08a687d0327aa06cafe43a
Jieun Lim <jieun776121@gmail.com>
2024-01-19 14:20:24 +0900

    반복html코드 컴포넌트화 & map 이용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap'
+import { Button, Container, Nav, Navbar, Row, Col } from 'react-bootstrap'
 import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import 상품데이터 from './data'
 
 function App() {
+
+  let [shoes] = useState(상품데이터)
+  // let [shoesImg] = useState([
+  //   "https://codingapple1.github.io/shop/shoes1.jpg",
+  //   "https://codingapple1.github.io/shop/shoes2.jpg",
+  //   "https://codingapple1.github.io/shop/shoes3.jpg",
+  // ])
+
   return (
     <div className="App">
-      <Button variant="success">Success</Button>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">ReactMall</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+          <OffCanvasExample placement={'end'} name={'세부메뉴'} />
+        </Container>
+      </Navbar>
 
-      <OffCanvasExample placement={'end'} name={'메뉴'} />
+      <div className='main-bg'></div>
+
+      <Row>
+        {
+          shoes.map(function (key, index) {
+            return (
+              <ShoeLists idx={index} shoesInfo={key} />
+            )
+          })
+        }
+      </Row>
     </div>
   );
 }
 
 export default App;
+
+function ShoeLists(props) {
+  return (
+    <Col sm>
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.idx + 1) + '.jpg'} width='80%' />
+      <h4>{props.shoesInfo.title}</h4>
+      <p>{props.shoesInfo.content}</p>
+      <p>{props.shoesInfo.price}</p>
+    </Col>
+  )
+}
 
 function OffCanvasExample({ name, ...props }) {
   const [show, setShow] = useState(false);
@@ -24,16 +63,22 @@ function OffCanvasExample({ name, ...props }) {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow} className="me-2">
+      <Button style={{
+        background: '#CCCCCC',
+        color: 'black',
+        border: 'none'
+      }}
+        variant="primary" onClick={handleShow} className="me-2">
         {name}
       </Button>
       <Offcanvas show={show} onHide={handleClose} {...props}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+          <li>상의</li>
+          <li>하의</li>
+          <li>주얼리</li>
         </Offcanvas.Body>
       </Offcanvas>
     </>
