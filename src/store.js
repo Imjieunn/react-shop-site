89b118789cff960fa9c3f24c1911928d3c85d35e
Jieun Lim <jieun776121@gmail.com>
2024-01-30 14:54:04 +0900

    reudx store.js 추가, 초기세팅

---
+++ src/store.js
@@ -0,0 +1,29 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+// useState() 역할
+// state 하나를 slice라고 부른다
+let 상품명 = createSlice({
+    name: 'title',
+    initialState: ['White and Black', 'Grey Yordan']
+})
+
+let 담은갯수 = createSlice({
+    name: 'count',
+    initialState: [2, 1]
+})
+
+let id = createSlice({
+    name: 'id',
+    initialState: [0, 1]
+})
+
+
+export default configureStore({
+    reducer: {
+        // createSlice 한 것은 여기에 등록해야 사용가능
+        // 이름(작명) : user.reducer
+        id: id.reducer,
+        상품명: 상품명.reducer,
+        담은갯수: 담은갯수.reducer
+    }
+})
