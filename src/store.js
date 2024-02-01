dc958c72b589fc5ad21c45f37755407e532bfac4
Jieun Lim <jieun776121@gmail.com>
2024-02-01 13:56:00 +0900

    redux 파일 분할

--- src/store.js
+++ src/store.js
@@ -1,38 +1,6 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
-
-// useState() 역할
-// state 하나를 slice라고 부른다
-let user = createSlice({
-    name: 'user',
-    initialState: 'jieun',
-    reducers: {
-        // changeName() {
-        //     return 'Im jieun'
-        // }
-
-        // 기존 state를 이용하고 싶다면,
-        changeName(state) {
-            return 'Im ' + state
-        }
-        // 함수 여러개 설정하고 싶으면 , 로 추가하기
-        // ,
-        // 함수2() {
-
-        // }
-    }
-})
-
-export let { changeName } = user.actions
-
-
-let cart = createSlice({
-    name: 'cart',
-    initialState: [
-        { id: 0, name: 'White and Black', count: 2 },
-        { id: 2, name: 'Grey Yordan', count: 1 }
-    ]
-})
-
+import user from './store/userSlice'
+import cart from './store/cartSlice'
 
 export default configureStore({
     reducer: {
