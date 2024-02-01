75bbd55405075478c8108e16d439b0275f13d5ce
Jieun Lim <jieun776121@gmail.com>
2024-02-01 13:53:56 +0900

    상품 수량 늘리기 & 특정 상품 카트에서 제거

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,7 @@
 import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { changeName } from "../store";
+import { increaseAge, changeName } from "../store/userSlice";
+import { increaseCount, deleteCart } from "../store/cartSlice";
 
 function Cart() {
 
@@ -9,19 +10,23 @@ function Cart() {
     let dispatch = useDispatch()
 
     return (
-        <>
-            {store_state.user}의 장바구니
+        <div className="container">
+            <h4>{store_state.user.name} {store_state.user.age}의 장바구니</h4>
+            <button onClick={() => { dispatch(increaseAge(10)) }}>나이 추가하기</button>
 
-            <Table striped bordered hover>
+            <Table striped bordered hover style={{ marginTop: '20px' }}>
                 <thead>
                     <tr>
-                        <th width="10%">#</th>
-                        <th width="70%">상품명</th>
+                        <th width="5%">#</th>
+                        <th width="55%">상품명</th>
                         <th width="20%">수량</th>
+                        <th width="10%">수량추가</th>
+                        <th width="10%">삭제하기</th>
                     </tr>
                 </thead>
                 <tbody>
                     {(store_state.cart).map(function (value, index) {
+                        console.log(value)
                         return (
                             < tr key={index} >
                                 <td>{value.id}</td>
@@ -29,15 +34,22 @@ function Cart() {
                                 <td>{value.count}</td>
                                 <td>
                                     <button onClick={() => {
-                                        dispatch(changeName())
+                                        dispatch(increaseCount(index))
                                     }}>+</button>
+                                </td>
+                                <td>
+                                    <button onClick={() => {
+                                        dispatch(deleteCart(index))
+                                    }}>
+                                        -
+                                    </button>
                                 </td>
                             </tr>
                         )
                     })}
                 </tbody>
             </Table >
-        </>
+        </div>
     )
 }
 
