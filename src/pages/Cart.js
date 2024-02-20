915dce2730ee4c6a304795332536a3011a5e88e9
Jieun Lim <jieun776121@gmail.com>
2024-02-20 23:14:06 +0900

    useMemo 실습

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,16 +1,26 @@
+import { memo, useState } from "react";
 import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseAge, changeName } from "../store/userSlice";
 import { increaseCount, deleteCart } from "../store/cartSlice";
+
+// 하위 컴포넌트의 재렌더링 조건 걸기 : memo (꼭 필요할 때만 재렌더링)
+let Child = memo(function () {
+    console.log('재렌더링됨')
+    return <div>자식임</div>
+})
 
 function Cart() {
 
     // Redux store 가져와줌
     let store_state = useSelector((state) => { return state })
     let dispatch = useDispatch()
+    let [count, setCount] = useState(0)
 
     return (
         <div className="container">
+            <Child></Child>
+            <button onClick={() => { setCount(count + 1) }}>+</button>
             <h4>{store_state.user.name} {store_state.user.age}의 장바구니</h4>
             <button onClick={() => { dispatch(increaseAge(10)) }}>나이 추가하기</button>
 
